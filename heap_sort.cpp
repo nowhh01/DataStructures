f861d7be76aaac9d3d932cd89aa6ea81e214ab53
nowhh01 <kyeongsigi@gmail.com>
2019-09-07 17:26:50 +0000

    Updated heap_sort

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -2,53 +2,62 @@
 #include <vector>
 #include <array>
 
-template<typename Comparable>
-void HeapSort(std::vector<Comparable>* const outData)
-{
-	for(size_t i = outData->size() / 2 - 1; i >= 0; i--)
-	{
-		Heapify(i, outData->size(), outData);
-	}
-
-	for(size_t i = outData->size() - 1; i > 0; i--)
-	{
-		std::swap(outData->at(0), outData->at(i));
-		Heapify(0, i, outData);
-	}
-}
-
-inline const size_t LeftChild(const size_t i)
+inline const int LeftChild(const int i)
 {
 	return 2 * i + 1;
 }
 
 template<typename Comparable>
-void Heapify(size_t i, const size_t size, std::vector<Comparable>* const outData)
+void Heapify(int i, const int size, std::vector<Comparable>* const outData)
 {
-	size_t child;
+	int child;
 	Comparable tmp;
 
 	for(tmp = std::move(outData->at(i)); LeftChild(i) < size; i = child)
 	{
 		child = LeftChild(i);
 
-		if(child != size - 1 && outData->at(child) < outData->at(child + 1))
+		if(child != size - 1 && outData->at(child) < outData->at(child + 1))  // find which child is bigger
 		{
 			++child;
 		}
 
-		if(tmp < outData->at(child))
+		if(tmp < outData->at(child))  // check if child is bigger than parent
 		{
-			outData(i) = std::move(outData->at(child));
+			outData->at(i) = std::move(outData->at(child));
 		}
 		else
 		{
 			break;
 		}
 	}
+	
 	outData->at(i) = std::move(tmp);
+}
+
+template<typename Comparable>
+void HeapSort(std::vector<Comparable>* const outData)
+{
+	for(int i = outData->size() / 2 - 1; i >= 0; --i)
+	{
+		Heapify(i, outData->size(), outData);
+	}
+
+	for(int i = outData->size() - 1; i > 0; --i)
+	{
+		std::swap(outData->at(0), outData->at(i));
+		Heapify(0, i, outData);
+	}
 }
 
 int main()
 {
+	std::vector<int> a1{ 8, 6, 4, 4, 1, 9, 2 };
+	HeapSort(&a1);
+
+	for(const auto& x : a1)
+	{
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
 }
